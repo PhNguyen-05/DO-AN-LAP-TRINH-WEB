3da210a060606f271f2765e2c93db08889e0b410
hoangkimdl <kimnguyenthihoang916@gmail.com>
2025-10-25 11:08:26 +0700

    xoá các file tiếng Việt cũ

--- src/main/java/vn/iotstar/starshop/repository/VanChuyenRepository.java
+++
@@ -1,7 +0,0 @@
-package vn.iotstar.starshop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vn.iotstar.starshop.entity.VanChuyen;
-
-public interface VanChuyenRepository extends JpaRepository<VanChuyen, Integer> {
-}
